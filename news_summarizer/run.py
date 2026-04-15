04274608bc4e36a2337d9ff0afa18bc254b0a7d0
jigshGitHub <jigsh@hotmail.com>
2026-04-15 17:53:58 +0000

    news summarize updated

--- news_summarizer/run.py
+++ news_summarizer/run.py
@@ -4,7 +4,10 @@ import os
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage
+from langchain.agents import create_agent
 
 # setup log information
 logging.basicConfig(
@@ -39,28 +42,108 @@ llm = ChatOpenAI(
 )
 logger.info(f"LLM initialized: model={gpt_model}, temperature=0.7")
 
+logger.info("Defining agent tools...")
+@tool
+def extract_news_contents(contents: str) -> str:
+    """
+    Extract factual points,3-5 bullet points of verifiable facts (Who, What, Where, When, Why).
+    Use this tool FIRST when the user provides a news article contents.
+    Input should be the user's email contents or topic.
+    Returns extracted contents.
+    """
+    # logger.info(f"[Tool: extract_news_contents] Received contents: '{contents}'")
+
+    extract_prompt = PromptTemplate(
+        input_variables=["contents"],
+        template="""You are an objective, neutral news analyst specializing in factual synthesis.
+        Contents: {contents}
+        Task: Analyze the provided news article contents to extract core information. Do not add outside information or personal interpretation.        
+        Provide extracted structured output with following
+        Key Factual Points: 3-5 bullet points of verifiable facts (Who, What, Where, When, Why).
+        Key Stakeholders: List the primary organizations, public figures, or groups involved.
+        Important Data: Any specific numbers, percentages, dates, or financial figures.
+        Attributed Perspectives: If the article contains quotes or opinions, attribute them clearly (e.g., "Source X claimed...")."""
+    )
+
+    formatted_prompt = extract_prompt.format(contents=contents)
+    logger.info("[Tool: extract_news_contents] Sending prompt to LLM...")
+
+    response = llm.invoke(formatted_prompt)
+
+    logger.info("[Tool: extract_news_contents] Contents extracted successfully!")
+    return response.content
+
+@tool
+def summarize_news_contents(extractd_content: str) -> str:
+    """
+    Take extracted contents, create a 3–4 sentence news summary in simple language that anyone can understand — no jargon
+    Returns a concise, clear news summary
+    """
+    logger.info("[Tool: summarize_news_contents] Summarizing extracted news contents...")
+    summarize_prompt = PromptTemplate(
+        input_variables=["extractd_content"],
+        template="""You are a senior news editor specialized in high-density factual synthesis..
+        Contents: {extractd_content}
+        Task: Analyze the provided extracted content and generate a structured summary. Do not add outside information or personal interpretation.        
+        Output Format:
+        TL;DR (1 Sentence): A high-impact summary using the "Action-Impact-Context" formula.
+        Key Points: 3-5 concise bullet points of verifiable facts.
+        Important Data: List specific dates, figures, or names mentioned.
+        Stakeholders: Identify the primary parties involved.
+        Strict Constraints:
+        Use clear action verbs (e.g., "Launched," "Rejected," "Secured").
+        Remove all "fluff" like "The article discusses..." or "In this piece...".
+        Maintain strict neutrality; do not include interpretation or bias."""
+    )
+
+    formatted_prompt = summarize_prompt.format(extractd_content=extractd_content)
+    logger.info("[Tool: summarize_news_contents] Sending prompt to LLM for final sumarry...")
+
+    response = llm.invoke(formatted_prompt)
+
+    logger.info("[Tool: summarize_news_contents] Contents summarized successfully!")
+    return response.content
+
+tools = [extract_news_contents, summarize_news_contents]
+logger.info(f"Tools registered: {[t.name for t in tools]}")
+
+logger.info("Creating the agent...")
+SYSTEM_PROMPT = """
+Role: You are a News Automation Agent responsible for summarizing long news article contents in easy to read concise summary.
+Core Objective: When a user gives you news article contents, use your defined tools extract core facts, and provide a structured summary by following these steps.:
+First use extract_news_contents to extract verifiable facts (Who, What, Where, When, Why) while ignoring editorial bias. Then use summarize_news_contents
+to summarize contents in easy-to-read summary.
+Before outputting, think step-by-step to ensure the data is consistent across sources.
+"""
+agent_graph = create_agent(
+    model=llm,
+    tools=tools,
+    system_prompt=SYSTEM_PROMPT,
+    debug=True
+)
+
 def run_content_summarizer(news_contents: str) -> str:
     """
-    This function when invoked, it executes agent's ability to summarize long contents pf news article in simple language summary.
+    This function when invoked, it executes agent's ability to summarize long contents of news article in simple language summary.
     """
 
-    logger.info("=" * 60)
-    logger.info(f"PASTED NEWS ARTICLE CONTENTS: {news_contents}")
-    logger.info("=" * 60)
+    # logger.info("=" * 60)
+    # logger.info(f"PASTED NEWS ARTICLE CONTENTS: {news_contents}")
+    # logger.info("=" * 60)
     logger.info("Agent is now thinking... watch the tool-calling loop below!")
     logger.info("-" * 60)
 
-    # result = agent_graph.invoke(
-    #     {"messages": [HumanMessage(content=news_contents)]}
-    # )
+    result = agent_graph.invoke(
+        {"messages": [HumanMessage(content=news_contents)]}
+    )
 
-    # summarized_contents = result["messages"][-1].content
+    summarized_contents = result["messages"][-1].content
 
     logger.info("-" * 60)
     logger.info("Agent finished! Here's your summarized contents:")
     logger.info("=" * 60)
 
-    return news_contents # summarized_contents
+    return summarized_contents
 
 # MMain function
 if __name__ == "__main__":
