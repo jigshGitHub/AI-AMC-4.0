d372ffb3a87f04b6abb22709cf79985ba3f304d2
jigshGitHub <jigsh@hotmail.com>
2026-09-24 12:52:03 -0400

    Langchain configured with AZURE

--- langchain/testlangchain.py
+++ langchain/testlangchain.py
@@ -1,37 +1,32 @@
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain_core.tools import tool
 from langchain.agents import create_agent
-
+from azure.identity import DefaultAzureCredential
 import os
-
-@tool
-def get_weather() -> str:
-    """
-    Returns the weather forecast for a given location.
-    In a real implementation, this would call a weather API. For this example, it returns a static response.
-    """
-    return f"The weather is 72°F and sunny."
 
 def create_my_custom_agent(model_name: str = "openai:gpt-4o"):
     load_dotenv()
-    tools = [get_weather]
     env_type = os.getenv("ENVTYPE")
     if env_type == "azure":
         from langchain_azure_ai.chat_models import AzureAIOpenAIApiChatModel
-        llm = AzureAIOpenAIApiChatModel(
-            project_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-            credential=None,  # Assuming DefaultAzureCredential is set up in the environment
-            model=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
-            temperature=0.7,
-            verbose=True,
-        )
-        agent = create_agent(
-            model=llm,
-            tools=tools,
-            system_prompt="You are a helpful assistant"
-        )
+        if os.getenv("AZURE_CREDENTIAL") == 'default':
+            llm = AzureAIOpenAIApiChatModel(
+                project_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+                credential = DefaultAzureCredential(exclude_environment_credential=True, exclude_managed_identity_credential=True) ,
+	            model=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
+                temperature=0.7,
+                verbose=True,
+            )
+        else:
+            # key credential
+            llm = AzureAIOpenAIApiChatModel(
+                endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+                credential=os.getenv("AZURE_OPENAI_API_KEY"),
+                model=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
+                temperature=0.7,
+                verbose=True,
+            )
     else:
         model_name = os.getenv("LLM_MODEL")
         llm = ChatOpenAI(
@@ -39,17 +34,18 @@ def create_my_custom_agent(model_name: str = "openai:gpt-4o"):
             temperature=0.7,
             verbose=True,
         )
-        agent = create_agent(
-            model=llm,
-            tools=tools,
-            system_prompt="You are a helpful and concise weather assistant."
-        )
+
+    agent = create_agent(
+        model=llm,
+        system_prompt="You are a helpful assistant."
+    )
     return agent
 
 
 if __name__ == "__main__":
     my_agent = create_my_custom_agent()
-    inputs = {"messages": [{"role": "user", "content": "What's the weather in Washington DC?"}]}
+    #inputs = {"messages": [{"role": "user", "content": "What's the weather in Washington DC?IS it good time to visit this year"}]}
+    inputs = {"messages": [{"role": "user", "content": "Who is AB in Bollywood?"}]}
 
     # for chunk in my_agent.stream(inputs, stream_mode="updates"):
     #     print(chunk)
